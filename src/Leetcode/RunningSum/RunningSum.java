c5018590952c0864bcf554221bea701562670a6e
DavidTsyganov <60532779+DavidTsyganov@users.noreply.github.com>
2021-11-16 09:46:16 +0700

    Update RunningSum.java

--- src/Leetcode/RunningSum/RunningSum.java
+++ src/Leetcode/RunningSum/RunningSum.java
@@ -5,24 +5,41 @@ import java.util.Scanner;
 
 public class RunningSum {
     public static void main(String[] args) {
+        final int[] nums = readInputs();
+        final int[] result = countSum(nums);
+        printInput(nums);
+        printResult(result);
+    }
+
+    private static int[] readInputs() {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите количество элементов в массиве");
         int n = scanner.nextInt();
+        int[] input = new int[n];
 
-        int[] nums = new int[n];
-        int[] runningSum = new int[n];
-        int sum = 0;
-
-        for (int i = 0; i < nums.length; ++i) {
-            nums[i] = (int) (Math.random() * 100);
+        for (int i = 0; i < input.length; ++i) {
+            input[i] = (int) (Math.random() * 100);
         }
+        return input;
+    }
+
+    private static int[] countSum(int[] nums) {
+        int[] runningSum = new int[nums.length];
+        int sum = 0;
 
         for (int j = 0; j < runningSum.length; ++j) {
             sum += nums[j];
             runningSum[j] = sum;
         }
+        return runningSum;
+    }
 
-        System.out.println("Исходный массив: " + Arrays.toString(nums));
-        System.out.println("Итоговый массив: " + Arrays.toString(runningSum));
+    private static void printInput(int[] input) {
+        System.out.println("Исходный массив: " + Arrays.toString(input));
+    }
+
+    private static void printResult(int[] result) {
+        System.out.println("Итоговый массив: " + Arrays.toString(result));
     }
 }
